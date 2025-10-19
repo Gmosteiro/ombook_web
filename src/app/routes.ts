f16d8637910cf9e542a26e5b8d33e8777ad1f0ee
Gmosteiro <gastonmosteiro@hotmail.com>
2025-10-18 22:50:37 -0300

    Update pages

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,15 +5,19 @@ import {
     layout,
 } from "@react-router/dev/routes";
 
+
+const FEATURES = "../features";
+
 export default [
     // Public landing page
-    index("./pages/Landing.tsx"), // Página pública en "/"
-    route("login", "../features/auth/components/Login.tsx"),
-    route("forgot-password", "../features/auth/components/ForgotPassword.tsx"),
+    index(`${FEATURES}/common/pages/Landing.tsx`), // Página pública en "/"
+    route("login", `${FEATURES}/auth/components/Login.tsx`),
+    route("forgot-password", `${FEATURES}/auth/components/ForgotPassword.tsx`),
 
     // Private routes wrapped in a layout
-    layout("../features/auth/components/PrivateRoute.tsx", [
-        route("home", "./pages/Home.tsx"), // Home privado en "/home"
-        route("usuarios/alta", "../features/users/pages/UserCreatePage.tsx"),
+    layout(`${FEATURES}/auth/components/PrivateRoute.tsx`, [
+        route("home", `${FEATURES}/common/pages/Home.tsx`), // Home privado en "/home"
+        route("usuarios/alta", `${FEATURES}/users/pages/UserCreatePage.tsx`),
     ]),
 ] satisfies RouteConfig;
+
