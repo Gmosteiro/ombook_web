bebfccd194fa097de6d2886c83bb04a9095601e8
Gmosteiro <gastonmosteiro@hotmail.com>
2025-12-01 20:19:16 -0300

    reset password

--- src/tests/all_users/reset-password.spec.ts
+++ src/tests/all_users/reset-password.spec.ts
@@ -7,13 +7,14 @@ test('test', async ({ page }) => {
     await page.getByRole('button', { name: 'Abrir menú de usuario' }).click();
     await page.getByRole('navigation').getByRole('link', { name: 'Mi Perfil' }).click();
     await page.getByRole('button', { name: 'Editar' }).click();
-    await page.getByRole('link', { name: 'Cambiar Contraseña' }).click();
+    await page.getByRole('button', { name: 'Acciones' }).click();
+    await page.getByRole('button', { name: 'Cambiar Contraseña' }).click();
     await page.getByRole('textbox', { name: 'Contraseña Actual' }).click();
     await page.getByRole('textbox', { name: 'Contraseña Actual' }).fill(PROFESOR_USER.password);
-    await page.getByRole('textbox', { name: 'Nueva Contraseña', exact: true }).click();
+    await page.getByRole('textbox', { name: 'Contraseña Actual' }).press('Tab');
     await page.getByRole('textbox', { name: 'Nueva Contraseña', exact: true }).fill(PROFESOR_USER.password + '1');
     await page.getByRole('textbox', { name: 'Confirmar Nueva Contraseña' }).click();
     await page.getByRole('textbox', { name: 'Confirmar Nueva Contraseña' }).fill(PROFESOR_USER.password + '1');
     await page.getByRole('button', { name: 'Cambiar Contraseña' }).click();
-    await page.getByText('Contraseña actualizada').click();
+    await page.locator('div').filter({ hasText: /^Contraseña actualizada correctamente\.$/ }).click();
 });
