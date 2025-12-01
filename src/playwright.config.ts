38527b5071030497faadcf81245afd2224e09188
Gmosteiro <gastonmosteiro@hotmail.com>
2025-12-01 19:42:22 -0300

    feat: implement login helper function and update login test case

--- src/playwright.config.ts
+++ src/playwright.config.ts
@@ -1,5 +1,9 @@
-testDir: 'tests'
-use: {
-  browserName: 'chromium',
-  headless: true
-}
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: 'tests',
+  use: {
+    browserName: 'chromium',
+    headless: true,
+  },
+});
---
+++ src/tests/all_users/helpers.ts
@@ -0,0 +1,13 @@
+import { Page } from '@playwright/test';
+import { WEB_PAGE } from '../playwright.constants';
+
+export async function login(page: Page, email: string, password: string) {
+    await page.goto(`${WEB_PAGE}/login/`);
+    await page.getByText('Correo electrónicoContraseñ').click();
+    await page.getByRole('textbox', { name: 'Correo electrónico' }).click();
+    await page.getByRole('textbox', { name: 'Correo electrónico' }).fill(email);
+    await page.getByRole('textbox', { name: 'Correo electrónico' }).press('Tab');
+    await page.getByRole('textbox', { name: 'Contraseña' }).fill(password);
+    await page.getByRole('button', { name: 'Iniciar Sesión' }).click();
+    await page.getByRole('heading', { name: '¡Bienvenido a Ombook Gaston' }).click();
+}
