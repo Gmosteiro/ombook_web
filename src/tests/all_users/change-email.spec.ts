02b61abed4204957f9260683882b8ceb06f56b93
Gmosteiro <gastonmosteiro@hotmail.com>
2025-12-01 20:16:31 -0300

    change email

--- src/tests/all_users/change-email.spec.ts
+++ src/tests/all_users/change-email.spec.ts
@@ -1,13 +1,19 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
+import { login } from './helpers';
+import { ESTUDIANTE_USER } from '../playwright.constants';
 
 test('test', async ({ page }) => {
-    await page.goto('http://localhost:3000/login/');
-    await page.getByRole('textbox', { name: 'Correo electrónico' }).click();
-    await page.getByRole('textbox', { name: 'Correo electrónico' }).fill('gastonmosteiro@hotmail.com');
-    await page.getByRole('textbox', { name: 'Contraseña' }).click();
-    await page.getByRole('textbox', { name: 'Contraseña' }).fill('Test.1234');
-    await page.getByRole('button', { name: 'Iniciar Sesión' }).click();
+    await login(page, ESTUDIANTE_USER.mail, ESTUDIANTE_USER.password);
+
     await page.getByRole('button', { name: 'Abrir menú de usuario' }).click();
     await page.getByRole('navigation').getByRole('link', { name: 'Mi Perfil' }).click();
     await page.getByRole('button', { name: 'Editar' }).click();
+    await page.getByRole('button', { name: 'Acciones' }).click();
+    await page.getByRole('button', { name: 'Cambiar Correo' }).click();
+    await page.getByRole('textbox', { name: 'Nuevo correo electrónico' }).click();
+    await page.getByRole('textbox', { name: 'Nuevo correo electrónico' }).fill('test@test.com');
+    await page.getByRole('textbox', { name: 'Nuevo correo electrónico' }).press('Tab');
+    await page.getByRole('textbox', { name: 'Contraseña actual' }).fill(ESTUDIANTE_USER.password);
+    await page.getByRole('button', { name: 'Solicitar cambio de correo' }).click();
+    await page.getByText('Solicitud de cambio de correo').click();
 });
