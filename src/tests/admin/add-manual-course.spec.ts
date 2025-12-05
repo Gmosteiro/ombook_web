4c0238ad4d361d115c1488ba8fd4fa8cfe853f3b
AlexRibm <117485971+AlexRibm@users.noreply.github.com>
2025-12-05 20:48:50 -0300

    correccion

--- src/tests/admin/add-manual-course.spec.ts
+++ src/tests/admin/add-manual-course.spec.ts
@@ -1,15 +1,9 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
+import { login } from '../all_users/helpers';
+import { ADMIN_USER } from '../playwright.constants';
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/login/');
-  await page.getByRole('textbox', { name: 'Correo electrónico' }).click();
-  await page.getByRole('textbox', { name: 'Correo electrónico' }).fill('prof1');
-  await page.getByRole('textbox', { name: 'Correo electrónico' }).press('Alt+6');
-  await page.getByRole('textbox', { name: 'Correo electrónico' }).press('Alt+4');
-  await page.getByRole('textbox', { name: 'Correo electrónico' }).fill('prof1@ombook.com');
-  await page.getByRole('textbox', { name: 'Contraseña' }).click();
-  await page.getByRole('textbox', { name: 'Contraseña' }).fill('Test.1234');
-  await page.getByRole('button', { name: 'Iniciar Sesión' }).click();
+  await login(page, ADMIN_USER.mail, ADMIN_USER.password);
   await page.getByRole('link', { name: 'Mis Cursos', exact: true }).click();
   await page.getByRole('button', { name: 'Ver Detalles' }).nth(4).click();
   await page.getByRole('link', { name: 'Usuarios' }).click();
